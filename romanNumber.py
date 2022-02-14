d187fd216169c3721d4c55541b63aa24ca576541
22cath <cecollignon@gmail.com>
2022-02-14 23:51:53 +0100

    fin de clase Romanas con objeto, clase, instancias, atributos y metodos

--- romanNumber.py
+++ romanNumber.py
@@ -1,6 +1,6 @@
-from romanas import arabigo_a_romano, romano_a_arabigo, RomanError
+from romanos import arabigo_a_romano, romano_a_arabigo, RomanError
 
-class RomanNumber():
+class RomanNumber(object):
     def __init__(self, entrada):
         if isinstance(entrada, str):
             self.arabigo = romano_a_arabigo(entrada)
@@ -9,6 +9,55 @@ class RomanNumber():
             self.romano = arabigo_a_romano(entrada)
             self.arabigo = entrada
         else:
-            raise TypeError('La entrada tienes que ser entero')
-        
+            raise TypeError("La entrada tiene que ser entero o cadena")
+
+    def __repr__(self):
+        return self.romano
+
+    def __str__(self):
+        return self.romano
+
+    def __int__(self):
+        return self.arabigo
+
+    def __lt__(self, entrada):
+        if not isinstance(entrada, RomanNumber):
+            raise TypeError("entrada tiene que ser romano")
+        return self.arabigo < entrada.arabigo
+
+    def __le__(self, other):
+        if not isinstance(other, RomanNumber):
+            raise TypeError("entrada tiene que ser romano")
+        return self.arabigo <= other.arabigo
+
+
+    def __eq__(self, other):
+        if not isinstance(other, RomanNumber):
+            raise TypeError("entrada tiene que ser romano")
+        return self.arabigo == other.arabigo
+
+    def __ne__(self, other):
+        if not isinstance(other, RomanNumber):
+            raise TypeError("entrada tiene que ser romano")
+        return self.arabigo != other.arabigo
+
+    def __gt__(self, other):
+        if not isinstance(other, RomanNumber):
+            raise TypeError("entrada tiene que ser romano")
+        return self.arabigo > other.arabigo
+
+    def __ge__(self, other):
+        if not isinstance(other, RomanNumber):
+            raise TypeError("entrada tiene que ser romano")
+        return self.arabigo >= other.arabigo
+
+    def __add__(self, other):
+        if not isinstance(other, RomanNumber):
+            raise TypeError("entrada tiene que ser romano")
+
+        suma = self.arabigo + other.arabigo
+        resultado = RomanNumber(suma)
+        return resultado
+
+        return RomanNumber(self.arabigo + other.arabigo)
         
